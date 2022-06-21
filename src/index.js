f7cc25fce17429558af234bc4e1aa93b2ca29f05
regmis <sandeshreg35@gmail.com>
2022-06-20 22:40:20 -0500

    middleware and file rearrangement

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import 'dotenv/config';
 import Express from 'express';
 import Cors from 'cors';
 import Helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
+import Morgan from 'morgan';
+
+const limiter = RateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100, 
+    standardHeaders: true,
+    legacyHeaders: false, 
+})
 
 const app = Express();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}`));
-
 app.use(Cors())
 app.use(Helmet())
+app.use(Morgan('tiny'))
+app.use(Express.json())
+app.use(limiter)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
